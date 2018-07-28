0b544b3163a92fcc59f1fc4538fa43c9334cd463
JS5028452 <shaik.jakeerhussain217@gmail.com>
2018-07-28 13:17:27 +0530

    First commit

---
+++ FirstMvn/src/test/java/FirstMvn/FirstMvn/AppTest.java
@@ -0,0 +1,22 @@
+package FirstMvn.FirstMvn;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+    
+	@Test
+    public void appTest()
+    {
+        Assert.assertTrue(false);
+        Assert.assertTrue(true);
+    }
+
+    
+   
+}
